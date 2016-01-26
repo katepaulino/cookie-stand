8c5a682cea3137bca75c74eeb201d66ad568f328
Kate Lucas <katherinelucas@gmail.com>
2016-01-25 18:16:50 -0800

    Adding style guide html and css

--- app.js
+++ app.js
@@ -1,205 +1,52 @@
 var hoursOpen = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
-// Pike Place Store
-var pikePlace = {
-  name: "Pike Place",
-  min: 17,
-  max: 88,
-  avgCookie: 5.2,
-  hourlySales: [],
-  totalSales: 0,
+function CookieStore(name, hoursOpen, min, max, avg) {
+  this.name = name;
+  this.minCustomer = min;
+  this.maxCustomer = max;
+  this.avgCookie = avg;
+  this.hoursOpen = hoursOpen;
+  this.hourlySales = [];
+  this.totalSales = 0;
+}
 
-  getRandomCust: function() {
-    return Math.random() * (this.max - this.min) + this.min;
-  },
+CookieStore.prototype.getRandomcust = function() {
+  return Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer;
+}
 
-  getHourlySales: function () {
-    for (var i = 0; i< hoursOpen.length; i++) {
-      var rand = Math.floor(this.getRandomCust() * this.avgCookie);
-      this.hourlySales.push(rand);
-      this.totalSales += rand;
-    }
-  },
-render: function() {
-  this.getHourlySales();
-  var sectionEl = document.getElementById("pikePlaceStore");
-  var ulEl = document.createElement("ul");
-
-  for (var i = 0; i < hoursOpen.length; i++) {
-    var liEl = document.createElement("li");
-    liEl.textContent = hoursOpen[i] + ": " + this.hourlySales[i];
-    ulEl.appendChild(liEl);
+CookieStore.prototype.getHourlySales = function () {
+  for (var i = 0; i< this.hoursOpen.length; i++) {
+    var rand = Math.floor(this.getRandomcust() * this.avgCookie);
+    this.hourlySales.push(rand);
+    this.totalSales += rand;
   }
-  var liElTotal = document.createElement("li");
-  liElTotal.textContent = "Total: " + this.totalSales;
-  ulEl.appendChild(liElTotal);
-  sectionEl.textContent = this.name;
-  sectionEl.appendChild(ulEl);
-  },
-};
+}
 
-pikePlace.render();
-// end of Pike Place Store
+var pikePlace = new CookieStore ('Pike Place', 8, 17, 88, 5.2);
+var seaTac = new CookieStore ('SeaTac Airport', 8, 6, 24, 1.2);
+var southcenter = new CookieStore ('Southcenter', 8, 11, 38, 1.9);
+var bellevueSquare = new CookieStore ('Bellevue Square', 8, 20, 48, 3.3);
+var alki = new CookieStore ('Alki', 8, 3, 24, 2.6);
 
-// SeaTac Airport Store
-var seaTac = {
-  name: "SeaTac Airport",
-  min: 6,
-  max: 24,
-  avgCookie: 1.2,
-  hourlySales: [],
-  totalSales: 0,
+CookieStore.prototype.render = function() {
+    this.getHourlySales();
+    var sectionEl = document.getElementById("pikePlaceStore");
+    var ulEl = document.createElement("ul");
 
-  getRandomCust: function() {
-    return Math.random() * (this.max - this.min) + this.min;
-  },
-
-  getHourlySales: function () {
-    for (var i = 0; i< hoursOpen.length; i++) {
-      var rand = Math.floor(this.getRandomCust() * this.avgCookie);
-      this.hourlySales.push(rand);
-      this.totalSales += rand;
+    for (var i = 0; i < hoursOpen.length; i++) {
+      var liEl = document.createElement("li");
+      liEl.textContent = hoursOpen[i] + ": " + this.hourlySales[i];
+      ulEl.appendChild(liEl);
     }
-  },
-render: function() {
-  this.getHourlySales();
-  var sectionEl = document.getElementById("seaTacStore");
-  var ulEl = document.createElement("ul");
-
-  for (var i = 0; i < hoursOpen.length; i++) {
-    var liEl = document.createElement("li");
-    liEl.textContent = hoursOpen[i] + ": " + this.hourlySales[i];
-    ulEl.appendChild(liEl);
+    var liElTotal = document.createElement("li");
+    liElTotal.textContent = "Total: " + this.totalSales;
+    ulEl.appendChild(liElTotal);
+    sectionEl.textContent = this.name;
+    sectionEl.appendChild(ulEl);
   }
-  var liElTotal = document.createElement("li");
-  liElTotal.textContent = "Total: " + this.totalSales;
-  ulEl.appendChild(liElTotal);
-  sectionEl.textContent = this.name;
-  sectionEl.appendChild(ulEl);
-  },
-};
 
-seaTac.render();
-// end of SeaTac Store
-
-// Southcenter Store
-var southcenter = {
-  name: "Southcenter Store",
-  min: 11,
-  max: 38,
-  avgCookie: 1.9,
-  hourlySales: [],
-  totalSales: 0,
-
-  getRandomCust: function() {
-    return Math.random() * (this.max - this.min) + this.min;
-  },
-
-  getHourlySales: function () {
-    for (var i = 0; i< hoursOpen.length; i++) {
-      var rand = Math.floor(this.getRandomCust() * this.avgCookie);
-      this.hourlySales.push(rand);
-      this.totalSales += rand;
-    }
-  },
-render: function() {
-  this.getHourlySales();
-  var sectionEl = document.getElementById("southcenterStore");
-  var ulEl = document.createElement("ul");
-
-  for (var i = 0; i < hoursOpen.length; i++) {
-    var liEl = document.createElement("li");
-    liEl.textContent = hoursOpen[i] + ": " + this.hourlySales[i];
-    ulEl.appendChild(liEl);
-  }
-  var liElTotal = document.createElement("li");
-  liElTotal.textContent = "Total: " + this.totalSales;
-  ulEl.appendChild(liElTotal);
-  sectionEl.textContent = this.name;
-  sectionEl.appendChild(ulEl);
-  },
-};
-
-southcenter.render();
-// end of Southcenter Store
-
-// Bellevue Square Store
-var bellevueSquare = {
-  name: "Bellevue Square Store",
-  min: 11,
-  max: 48,
-  avgCookie: 3.3,
-  hourlySales: [],
-  totalSales: 0,
-
-  getRandomCust: function() {
-    return Math.random() * (this.max - this.min) + this.min;
-  },
-
-  getHourlySales: function () {
-    for (var i = 0; i< hoursOpen.length; i++) {
-      var rand = Math.floor(this.getRandomCust() * this.avgCookie);
-      this.hourlySales.push(rand);
-      this.totalSales += rand;
-    }
-  },
-render: function() {
-  this.getHourlySales();
-  var sectionEl = document.getElementById("bellevueSquareStore");
-  var ulEl = document.createElement("ul");
-
-  for (var i = 0; i < hoursOpen.length; i++) {
-    var liEl = document.createElement("li");
-    liEl.textContent = hoursOpen[i] + ": " + this.hourlySales[i];
-    ulEl.appendChild(liEl);
-  }
-  var liElTotal = document.createElement("li");
-  liElTotal.textContent = "Total: " + this.totalSales;
-  ulEl.appendChild(liElTotal);
-  sectionEl.textContent = this.name;
-  sectionEl.appendChild(ulEl);
-  },
-};
-
-bellevueSquare.render();
-// End of Bellevue Square Store
-
-// Alki Store
-var alki = {
-  name: "Alki Store",
-  min: 3,
-  max: 24,
-  avgCookie: 2.6,
-  hourlySales: [],
-  totalSales: 0,
-
-  getRandomCust: function() {
-    return Math.random() * (this.max - this.min) + this.min;
-  },
-
-  getHourlySales: function () {
-    for (var i = 0; i< hoursOpen.length; i++) {
-      var rand = Math.floor(this.getRandomCust() * this.avgCookie);
-      this.hourlySales.push(rand);
-      this.totalSales += rand;
-    }
-  },
-render: function() {
-  this.getHourlySales();
-  var sectionEl = document.getElementById("alkiStore");
-  var ulEl = document.createElement("ul");
-
-  for (var i = 0; i < hoursOpen.length; i++) {
-    var liEl = document.createElement("li");
-    liEl.textContent = hoursOpen[i] + ": " + this.hourlySales[i];
-    ulEl.appendChild(liEl);
-  }
-  var liElTotal = document.createElement("li");
-  liElTotal.textContent = "Total: " + this.totalSales;
-  ulEl.appendChild(liElTotal);
-  sectionEl.textContent = this.name;
-  sectionEl.appendChild(ulEl);
-  },
-};
-
-alki.render();
+  pikePlace.render();
+  seaTac.render();
+  southcenter.render();
+  bellevueSquare.render();
+  alki.render();
